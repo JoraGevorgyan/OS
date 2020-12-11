55f7803143c65db69a03f312900b624c740a9215
Jora Gevorgyan <joragevorgyan004@gmail.com>
2020-12-12 01:42:23 +0400

    erased useless files

--- homework2/source/fileWorkLibrary.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once       // test, if this header file already difined in this project, do not it again
-#include <ostream>  // for usinf ostream iterator in printFileSize function
-
-int fileCopy(const char*, const char*);
-int printFileSize(const char*, std::ostream&);
